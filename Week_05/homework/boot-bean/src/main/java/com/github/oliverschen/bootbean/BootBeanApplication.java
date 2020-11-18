cca2dcc19ddbdd6cf651447b0473563276d14699
chenkui <jihechen@chinaesport.com>
2020-11-18 20:26:46 +0800

    boot-starter

---
+++ Week_05/homework/boot-bean/src/main/java/com/github/oliverschen/bootbean/BootBeanApplication.java
@@ -0,0 +1,7 @@
+package com.github.oliverschen.bootbean;
+
+/**
+ * @author ck
+ */
+public class BootBeanApplication {
+}
---
+++ Week_05/homework/boot-bean/src/test/java/com/github/oliverschen/bootbean/BootBeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.oliverschen.bootbean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootBeanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
