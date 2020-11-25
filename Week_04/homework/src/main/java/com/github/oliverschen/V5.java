f02c455faf7d7a625f305d37cfd8ebaba021ed97
chenkui <jihechen@chinaesport.com>
2020-11-25 10:35:33 +0800

    semaphore优化

--- Week_04/homework/src/main/java/com/github/oliverschen/V5.java
+++ Week_04/homework/src/main/java/com/github/oliverschen/V5.java
@@ -5,7 +5,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @author ck
- * Semaphore 实现，这个实现感觉不是很理想，最后使用了 join() 操作
+ * Semaphore 实现
  */
 public class V5 extends AbstractBase {
 
@@ -15,21 +15,19 @@ public class V5 extends AbstractBase {
 
     @Override
     public int asyncInvoke() throws Exception {
-        Semaphore semaphore = new Semaphore(1);
+        Semaphore s = new Semaphore(0);
         AtomicInteger result = new AtomicInteger();
-        Thread thread = new Thread(() -> {
-            try {
-                semaphore.acquire(1);
-                result.set(sum());
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-                Thread.currentThread().interrupt();
-            } finally {
-                semaphore.release();
-            }
-        });
-        thread.start();
-        thread.join();
+        new Thread( () -> {
+            result.set(sum());
+            s.release();
+        } ).start();
+        try {
+            s.acquire();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+            Thread.currentThread().interrupt();
+        }
         return result.get();
+
     }
 }
