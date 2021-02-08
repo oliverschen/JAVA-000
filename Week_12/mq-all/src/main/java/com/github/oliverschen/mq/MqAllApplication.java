100a6578e4c1719ca5e66aa45aea61f529226c3d
chenkui <jihe12180@gmail.com>
2021-02-08 22:13:15 +0800

    change dir name

---
+++ Week_12/mq-all/src/main/java/com/github/oliverschen/mq/MqAllApplication.java
@@ -0,0 +1,13 @@
+package com.github.oliverschen.mq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqAllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MqAllApplication.class, args);
+    }
+
+}
---
+++ Week_12/mq-all/src/test/java/com/github/oliverschen/mq/MqAllApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.oliverschen.mq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqAllApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
