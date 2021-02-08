43f8b1f02481e09718008847435fd598c34f4396
chenkui <jihe12180@gmail.com>
2021-02-08 11:55:10 +0800

    activemq test

---
+++ Week_12/active-mq-jms/src/main/java/com/github/oliverschen/mq/ActiveMqJmsApplication.java
@@ -0,0 +1,13 @@
+package com.github.oliverschen.mq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActiveMqJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ActiveMqJmsApplication.class, args);
+    }
+
+}
---
+++ Week_12/active-mq-jms/src/test/java/com/github/oliverschen/mq/ActiveMqJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.oliverschen.mq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActiveMqJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
